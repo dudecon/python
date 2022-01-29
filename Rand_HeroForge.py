54cbc7d00c08c9d43b8dcd39941b84ab428c0363
Paul Spooner <dudecon@hotmail.com>
2022-01-29 13:46:54 -0800

    Add files via upload

--- Rand_HeroForge.py
+++ Rand_HeroForge.py
@@ -4,7 +4,7 @@ import random, webbrowser
 # append the page number on the end of this address
 pre_address = "https://www.heroforge.com/load_share%3D"
 # the last page number (inclusive), page numbers start at 1
-page_last = 410736
+page_last = 411648
 
 random_page_number = random.choice(range(1, page_last + 1))
 full_address = pre_address + str(random_page_number) + '/'
