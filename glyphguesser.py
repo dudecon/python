5e0a0b32fd1de6e4c4df27fc8566e98b4f7769f3
Paul Spooner <dudecon@hotmail.com>
2022-11-11 12:26:52 -0800

    Better gameplay
    improved the glyph set and made the the hints more helpful

--- glyphguesser.py
+++ glyphguesser.py
@@ -1,28 +1,25 @@
 # Mastermind like game
 
-from random import choices, sample
+from random import choices, sample, shuffle
 from time import sleep
 
 DUPLICATES = False
-NumGlyphs = 36
-SeqLen = 4
+HOWMANY    = True
+WHICH      = False
+NumGlyphs  = 36
+SeqLen     = 3
+Guesses    = 0
 
-def compute_glyphs(BASE):
-    GLYPHS = []
-    BASE = min(BASE,1234)
-    offset = 55
-    for i in range(BASE):
-        if i < 10:
-            GLYPHS.append(str(i))
-            continue
-        if i == 36: offset += 101
-        elif i+offset == 888: offset += 2
-        elif i+offset == 896: offset += 5
-        elif i+offset == 907: offset += 4
-        elif i+offset == 930: offset += 2
-        elif i+offset == 1328: offset += 2
-        elif i+offset == 1367: offset += 11
-        GLYPHS.append(chr(i+offset))
+GLYPH_LIBRARY = '''0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿĀĂĄĆĈĊČĎĐĒĔĖĘĚĜĞĠĢĤĦĨĪĬĮİĲĴĶĸĺļľŀłńņňŊŌŎŐŒŔŖŘŚŜŞŠŢŤŦŨŪŬŮŰŲŴŶŸŹŻŽſƀƁƂƃƄƅƆƇƈƉƊƋƌƍƎƏƐƑƒƓƔƕƖƗƘƙƚƛƜƝƞƟƠơƢƣƤƥƦƧƨƩƪƫƬƭƮƯưƱƲƳƴƵƶƷƸƹƺƻƼƽƾƿǀǁǂǃǄǅǆǇǈǉǊǋǌǍǏǑǓǕǗǙǛǞǠǢǤǦǨǪǬǮǰǱǴǶǷǸǺǼǾȀȂȄȆȈȊȌȎȐȒȔȖȘȚȜȞȠȡȢȤȦȨȪȬȮȰȲȴȵȶȷȸȹȺȻȽȾȿɀɁɃɄɅɆɇɈɊɌɎɐɑɒɓɔɕɖɗɘəɚɛɜɝɞɟɠɡɢɣɤɥɦɧɨɩɪɫɬɭɮɯɰɱɲɳɴɵɶɷɸɹɺɻɼɽɾɿʀʁʂʃʄʅʆʇʈʉʊʋʌʍʎʏʐʑʒʓʔʕʖʗʘʙʚʛʜʝʞʟʠʡʢʣʤʥʦʧʨʩʪʫʬʭʮʯʰʱʲʳʴʵʶʷʸʹʺʻʼʽʾʿˀˁ˂˃˄˅ˆˇˈˉˊˋˌˍˎˏːˑ˒˓˔˕˖˗˘˙˚˛˝˞˟ˠˡˢˣˤˬ˭ˮ˯˰˱˲˳˴˵˶˷˸˹˺˻˼˽˾̵̅̆̌̍̎̏̐̑̾̿̀́͂̓̈́͊͋͌͛ͣͤͥͦͧͨͩͪͫͬͭͮͯ͜͟͢͝͞͠͡ͰͱͲͳʹ͵Ͷͷͺͻͼͽ;Ϳ΅Ά·ΈΉΊΏΐΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΤΥΦΧΨΩΪΫάέήίΰαβγδεζηθικλμνξοπρςστυφχψωϊϋόύώϏϐϑϒϓϔϕϖϗϘϚϜϞϠϢϤϥϦϧϨϪϬϮϰϱϲϳϴϵ϶ϷϸϹϺϻϼϽϾϿЀЁЂЃЄЅІЇЈЉЊЋЌЍЎЏАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯљњћќѝўџѠѡѢѣѤѥѦѧѨѩѪѫѬѭѮѯѰѱѲѳѴѵѶѷѸѹѺѻѼѽѾѿҀҁ҂҃҄҅҆҇҈҉ҊҌҎҐҒҔҖҘҚҜҞҠҢҤҦҨҪҬҮҰҲҴҶҸҺҼҾӀӁӃӅӇӉӋӍӐӒӔӖӘӚӜӞӠӢӤӦӨӪӬӮӰӲӴӶӸӺӼӾԀԂԄԆԈԊԌԎԐԒԔԖԘԚԜԞԠԢԤԨԪԬԮԲԳԴԵԶԷԸԹԺԻԼԽԾԿՀՁՂՃՄՅՆՇՈՉՊՋՌՍՎՏՐՑՒՓՔՕՖբգդեզէըթժիլխծկհձղճմյնշոչպջռսվտրցւփքօֆև६'''
+
+
+def compute_glyphs(num_g, offset=0, randomize=False):
+    end = min(len(GLYPH_LIBRARY),offset+num_g)
+    if randomize:
+        Shuffled = [i for i in GLYPH_LIBRARY]
+        shuffle(Shuffled)
+        GLYPHS = ''.join(Shuffled[offset:end])
+    else: GLYPHS =  GLYPH_LIBRARY[offset:end]
     return GLYPHS
 
 
@@ -34,7 +31,14 @@ def choose_sequence(seq_len, glyphs):
     return seq
 
 
-def isvalidguess(seq, seq_len, glyphs):
+def isvalidguess(seq, secret_sequence, glyphs, pr=False):
+    seq_len = len(secret_sequence)
+    numblyphs = len(glyphs)
+    if (numblyphs <= 10) and seq.isnumeric():
+        try:
+            seq = ''.join([glyphs[int(i)] for i in seq])
+            if pr: print(f'{seq} extracted from indicies')
+        except: pass
     guess_len = len(seq)
     if guess_len != seq_len:
         print(f"your guess was {guess_len} glyphs long,\n\
@@ -45,12 +49,22 @@ but the sequence is {seq_len} long")
             print(f"your guess contained {gl}\n\
 which is not (currently) a valid glyph")
             return False
-    return True
+    return seq
+
+
+def numcombos():
+    options = NumGlyphs
+    combos = NumGlyphs
+    for i in range(SeqLen-1):
+        if not DUPLICATES: options -= 1
+        combos *= options
+    return combos
+
 
 helpstr = '''Type stuff to guess the secret sequence
 # to show the glyph pool
-% to show how many glyphs in your last guess are in the sequence
-@ to show which glyphs from your last guess are in the sequence
+% to toggle showing how many glyphs you guess are in the sequence
+@ to toggle showing which glyphs you guess are in the sequence
 ! to disclose the sequence, and re-roll
 + to set the number of glyphs in the sequence, and re-roll
 * to set the size of the glyph pool, and re-roll
@@ -68,34 +82,75 @@ while True:
         print(''.join(allglyphs))
         print(f'the secret sequence is {SeqLen} glyphs long')
     elif cmd[0] == '%':
-        n = 0
-        for gl in lastguess:
-            if gl in secret_sequence: n += 1
-        print(f'your last guess contained {n} correct glyphs')
+        if HOWMANY:
+            HOWMANY = False
+            print('number of correct glyphs now hidden')
+        else:
+            HOWMANY = True
+            print('number of correct glyphs now shown')
     elif cmd[0] == '@':
-        corgl = ''
-        for gl in lastguess:
-            if gl in secret_sequence: corgl += gl
-        print(f'in your last guess {corgl} are in the sequence')
+        if WHICH:
+            WHICH = False
+            print('correct glyphs now hidden')
+        else:
+            WHICH = True
+            print('correct glyphs now shown')
     elif cmd[0] == '!':
         print(f'the secret sequence was {secret_sequence}')
         secret_sequence = choose_sequence(SeqLen,allglyphs)
     elif cmd[0] == '+':
         try:
             SeqLen = int(input("sequence should be how long? "))
+            SeqLen = max(SeqLen, 1)
             secret_sequence = choose_sequence(SeqLen,allglyphs)
             SeqLen = len(secret_sequence)
             print(f"sequence is now\n{SeqLen} glyphs long")
         except:
             print(f"errorǃ sequence is still\n{SeqLen} glyphs long")
     elif cmd[0] == '*':
+        print("how many glyphs? ")
+        print('follow by a space to select options below')
+        print('L to use just the Latin alphabet')
+        print('N to use just the alpha-numeric (default)')
+        print('G to use the Greek alphabet')
+        print('A to use the Armenian alphabet')        
+        print('C to use the Cyrillic alphabet')
+        print('R to use a random set of glyphs')
         try:
-            NumGlyphs = int(input("how many glyphs? "))
-            allglyphs = compute_glyphs(NumGlyphs)
+            selection = input("-> ").split()
+            NumGlyphs = int(selection[0])
+            NumGlyphs = max(NumGlyphs, 2)
+            if len(selection) > 1:
+                option = selection[1][0].upper()
+                if option == 'L':
+                    off = GLYPH_LIBRARY.find('A')
+                    NumGlyphs = min(NumGlyphs, 26)
+                    allglyphs = compute_glyphs(NumGlyphs, off)
+                elif option == 'N':
+                    off = 0
+                    NumGlyphs = min(NumGlyphs, 36)
+                    allglyphs = compute_glyphs(NumGlyphs, off)
+                elif option == 'G':
+                    off = GLYPH_LIBRARY.find('Α')
+                    NumGlyphs = min(NumGlyphs, 23)
+                    allglyphs = compute_glyphs(NumGlyphs, off)
+                elif option == 'C':
+                    off = GLYPH_LIBRARY.find('А')
+                    NumGlyphs = min(NumGlyphs, 32)
+                    allglyphs = compute_glyphs(NumGlyphs, off)
+                elif option == 'A': # Armenian
+                    off = GLYPH_LIBRARY.find('Բ')
+                    NumGlyphs = min(NumGlyphs, 36)
+                    allglyphs = compute_glyphs(NumGlyphs, off)
+                elif option == 'R':
+                    allglyphs = compute_glyphs(NumGlyphs, 0, True)
+                else: allglyphs = compute_glyphs(NumGlyphs)
+            else: allglyphs = compute_glyphs(NumGlyphs)
             NumGlyphs = len(allglyphs)
             secret_sequence = choose_sequence(SeqLen,allglyphs)
             SeqLen = len(secret_sequence)
-            print(f"there are now\n{NumGlyphs} glyphs")
+            print(f"there are now {NumGlyphs} glyphs:")
+            print(allglyphs)
         except:
             print(f"errorǃ there are still\n{NumGlyphs} glyphs")
     elif cmd[0] == '^':
@@ -105,17 +160,43 @@ while True:
         else:
             DUPLICATES = True
             print('the next sequence may contain duplicate glyphs')
-    elif isvalidguess(cmd, SeqLen, allglyphs):
-        lastguess = cmd
-        if cmd == secret_sequence:
-            print(f"Yes! {cmd} is correct! You win!")
+    elif isvalidguess(cmd, secret_sequence, allglyphs, False):
+        seq = isvalidguess(cmd, secret_sequence, allglyphs, True)
+        Guesses += 1
+        if seq == secret_sequence:
+            print(f"Yes! {seq} is correct! You win!")
             sleep(.5)
-            print('take a moment to celebrate your victory')
-            sleep(1)
+            bruteforce = numcombos()//2
+            if Guesses < bruteforce/4: qual = "just "
+            elif Guesses < bruteforce/2: qual = "only "
+            else:                      qual = ''
+            print(f"You guessed it in {qual}{Guesses} tries.")
+            print(f"It would take about {bruteforce} to bruteforce it.")
+            if Guesses < bruteforce/7:
+                print('Take a moment to celebrate your victory!')
+                sleep(1)
             print('Okay. New sequence generated.')
             secret_sequence = choose_sequence(SeqLen,allglyphs)
             sleep(.5)
             print('On with the game!')
-        else: print('sorry, try again')
+            lastguess = ''
+            Guesses = 0
+        else:
+            lastguess = seq
+            if WHICH:
+                corgl = ''
+                for gl in lastguess:
+                    if gl in secret_sequence: corgl += gl
+                print(f'in your guess {corgl} are in the sequence')
+                n = 0
+                for i in range(len(secret_sequence)):
+                    if secret_sequence[i] == lastguess[i]:
+                        n += 1
+                print(f'and {n} are in the correct location')
+            elif HOWMANY:
+                n = 0
+                for gl in lastguess:
+                    if gl in secret_sequence: n += 1
+                print(f'{n} of your glyphs are in the sequence')
     else: print(helpstr)
     cmd = input('-> ')
