fd279a303b7042b5806b7cb08793083499f98c77
Paul Spooner <dudecon@hotmail.com>
2022-11-18 15:21:53 -0800

    so much faster now

--- PrimeBatchFinder.py
+++ PrimeBatchFinder.py
@@ -1,87 +1,115 @@
-BATCHFOLDER = 'batch_primes'
-PREFIX = 'PrimesTo_'
-SAVESTRIDE = 500000  # re-build the files before changing.
-SUFFIX = '.txt'
-SAVEINTERVAL = 120  # seconds
-BATCHMASK = 83  # no work on batches below this number
-
 from time import time as tm
 from os import stat
-from multiprocessing import Process
+from multiprocessing import Process, Manager
+from time import sleep
+
+BATCHFOLDER = 'batch_primes'
+PREFIX = 'PrimesTo_'
+SAVESTRIDE = 500000  # re-build the files before changing, or nothing will load.
+STRIDESHARDS = 1000  # how many pieces to break each batch into for processing
+SUFFIX = '.txt'
+SAVEINTERVAL = 60  # seconds
+BATCHMASK = 1270  # no work on batches below this number
+NUMTHREADS = 8  # Number of threads to spawn
+
+SHARDLEN = ((SAVESTRIDE//STRIDESHARDS)//2)*2
 
 
-def mainfunc():
-    # Load moved inline
-    primes = []
-    cur_batch = -1
-    cur_limit = 0
-    increment = 2
+def loadbatch(cur_batch):
+    cur_limit = (cur_batch + 1) * SAVESTRIDE
+    savefile = f'{BATCHFOLDER}/{PREFIX}{cur_limit}{SUFFIX}'
+    try:
+        f = open(savefile, "r")
+        prime_source = f.read()
+        f.close()
+        loadedprimes = sorted([int(p) for p in prime_source.split() if p.isnumeric()])
+        return True, loadedprimes
+    except:
+        return False, [0]
+
+
+def isprime(candidate, primes):
+    for factor in primes:
+        if (candidate / factor) < factor: return True
+        if candidate % factor == 0: return False
+    raise  # because we should never get here
+
+
+def findprimes(foundationprimes, newprimes: list, start_candidate, end_of_search):
+    candidate = start_candidate
+    while candidate < end_of_search:  # prime finder loop
+        if isprime(candidate, foundationprimes):
+            newprimes.append(candidate)
+        candidate += 2
+
+
+def prime_main():
+    mngr = Manager()
+    newprimes = mngr.list()
+    loadsuccess, baseprimes = loadbatch(0)
+    if loadsuccess:
+        numinitprm = len(baseprimes)
+        print(f'initial batch of {numinitprm} primes loaded')
+    else:
+        print('unable to load first batch of primes')
+        print('bootstrapping not yet implemented')
+        return False
+    if baseprimes.pop(0) == 2:
+        print('2 successfully excised')
+    else:
+        print('Initial Prime Corrupted')
+        print('Revalidate data\nABORT!\nABORT!\nABORT!')
+        return False
+    cur_batch = BATCHMASK - 1
     while True:
         cur_batch += 1
-        batch_new = True  # to reserve a save file on the first prime found
         cur_limit = (cur_batch + 1) * SAVESTRIDE
         savefile = f'{BATCHFOLDER}/{PREFIX}{cur_limit}{SUFFIX}'
-        loaded = False
-        newprimes = []
-        try:
-            f = open(savefile, "r")
-            prime_source = f.read()
-            f.close()
-            gathered_primes = sorted([int(p) for p in prime_source.split() if p.isnumeric()])
-            primes += gathered_primes
-            del gathered_primes
-            del prime_source
-            loaded = True
-        except:
-            if len(primes) < 4:
-                primes = [2, 3, 5, 7]
-                loaded = True
-                newprimes = primes[:]
-            else: loaded = False
-        if cur_batch < BATCHMASK: continue
+        loaded, loadedprimes = loadbatch(cur_batch)
         if loaded:
-            # skip it if this file is being worked on recently
-            try:
-                if (stat(savefile).st_mtime + SAVEINTERVAL*1.618) > tm():
-                    print(f'Batch {cur_batch} appears to be already in work')
-                    continue
-            except: pass
-            candidate = primes[-1] + increment
-        else:  # The current batch was not loaded, so
-            candidate = cur_batch * SAVESTRIDE
-            if candidate % 2 == 0: candidate += 1
-        t: float = tm() + SAVEINTERVAL
-        while candidate < cur_limit:  # prime finder loop
-            primecheck = True
-            for factor in primes[1:]:
-                if (candidate / factor) < factor: break
-                if candidate % factor == 0:
-                    primecheck = False
-                    break
-            if primecheck:
-                print(candidate, "is prime")
-                primes.append(candidate)
-                newprimes.append(candidate)
-                # check if we need to save the file
-                if batch_new or (tm() > t):
-                    batch_new = False
-                    t = tm() + SAVEINTERVAL
-                    f = open(savefile, "a")
-                    for i in newprimes: f.write(str(i) + '\n')
-                    f.close()
-                    newprimes = []
-            candidate += increment
-        if len(newprimes):
-            # save out the last of the new primes
-            # before moving on to the next batch
+            shardstrt = loadedprimes[-1] + 2
+        else:  # The current batch was not loaded, so init from the batch number
+            shardstrt = cur_batch * SAVESTRIDE
+            if shardstrt % 2 == 0: shardstrt += 1
+        del loadedprimes
+        processes = []
+        savetm: float = tm() + SAVEINTERVAL
+        shardend = shardstrt
+        lastsavestart = shardstrt
+        curtm = tm()
+
+        def saveit(primestosave, processestowait):
+            while len(processestowait):
+                processestowait.pop(0).join()
+            sortedprimes = primestosave[:]
+            sortedprimes.sort()
+            elapsed = curtm - savetm + SAVEINTERVAL
+            numnewprimes = len(primestosave)
+            numproced = shardend - lastsavestart
+            print(f'saved {numnewprimes} from {numproced} numbers in {elapsed:>6.2f} seconds')
             f = open(savefile, "a")
-            for i in newprimes: f.write(str(i) + '\n')
+            for i in sortedprimes:
+                f.write(str(i) + '\n')
             f.close()
-            newprimes = []
+            return mngr.list()
+
+        while shardend < cur_limit:  # prime finder thread management
+            if len(processes) < NUMTHREADS:
+                shardend = min(shardstrt + SHARDLEN, cur_limit)
+                _prcs = Process(target=findprimes, args=(baseprimes, newprimes, shardstrt, shardend))
+                processes.append(_prcs)
+                _prcs.start()
+                shardstrt = min(shardstrt + SHARDLEN, cur_limit-1)
+            p = processes.pop(0)
+            if p.is_alive(): processes.append(p)
+            curtm = tm()
+            if curtm > savetm:
+                newprimes = saveit(newprimes, processes)
+                savetm: float = tm() + SAVEINTERVAL
+                lastsavestart = shardend + 2
+        newprimes = saveit(newprimes, processes)
         print(f'Primes in batch {cur_batch} completed')
 
 
 if __name__ == '__main__':
-    p = Process(target=mainfunc)
-    p.start()
-    p.join()
+    prime_main()
