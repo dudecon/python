a42294acd583a4517c101284ee7d03a945cb0187
Paul Spooner <dudecon@hotmail.com>
2023-06-02 12:10:56 -0700

    Update WedgeShapes.py
    Used ChatGPT to optimize the code.

--- WedgeShapes.py
+++ WedgeShapes.py
@@ -1,66 +1,38 @@
-# try to brute-force all the pie wedge orientation combinations
+# Try to brute-force all the pie wedge orientation combinations
 # (#,#,#,#) 0-3
 # 0 is in, 2 is out, 1, and 3 are rotated left and right
 
-
 def mirror(s):
-    # convert 1 to 3, and 3 to 1
-    # then reverse the direction
-    l = [x for x in s]
-    l.reverse()
-    for i, e in enumerate(l):
-        if e%2: l[i] = (e+2)%4
-    return tuple(l)
-
+    # Convert 1 to 3, and 3 to 1
+    # Then reverse the direction
+    return tuple((x + 2) % 4 if x % 2 else x for x in reversed(s))
 
 def rotate(s):
-    # shift the order one place
-    l = [x for x in s]
-    t = l.pop(0)
-    l.append(t)
-    return tuple(l)
+    # Shift the elements one place to the left
+    return s[1:] + (s[0],)
 
-
-# some test stuff
-# t = (0,1,0,0)
-# for i in range(8):
-#     t = rotate(t)
-#     print(t)
-
-allstates = set()
+allstates = []
 for i in range(4):
-    l = [i]
-    for i in range(4):
-        l.append(i)
-        for i in range(4):
-            l.append(i)
-            for i in range(4):
-                l.append(i)
-                allstates.add(tuple(l))
-                l.pop()
-            l.pop()
-        l.pop()
+    for j in range(4):
+        for k in range(4):
+            for l in range(4):
+                allstates.append((i, j, k, l))
 
-print(len(allstates))
-uniquestates = set()
-while len(allstates):
+uniquestates = []
+print(len(allstates), " total states")
+while allstates:
     s = allstates.pop()
-    uniquestates.add(s)
-    # remove all other permutations of s
-    for i in range(3):
-        s = rotate(s)
-        if s in allstates:
-            allstates.remove(s)
-    sm = mirror(s)
-    if sm != s:
-        if sm in allstates:
-            allstates.remove(sm)
-        for i in range(3):
-            sm = rotate(sm)
-            if sm in allstates:
-                allstates.remove(sm)
-print(len(uniquestates))
-l = list(uniquestates)
-l.sort()
-for i in l:
+    uniquestates.append(s)
+    # Remove all other permutations of s
+    allstates = [
+        x for x in allstates if x not in (
+            s, rotate(s), rotate(rotate(s)), rotate(rotate(rotate(s))),
+            mirror(s), mirror(rotate(s)), mirror(rotate(rotate(s))), mirror(rotate(rotate(rotate(s))))
+        )
+    ]
+
+uniquestates.sort()
+for i in uniquestates:
     print(i)
+
+print(len(uniquestates), " unique states")
