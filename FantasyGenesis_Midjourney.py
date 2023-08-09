cb280afd0d12529dd1a36b8f8f87deefc10d2a06
Paul Spooner <dudecon@hotmail.com>
2023-08-09 09:51:47 -0700

    Midjourney prompt fantasy genesis

---
+++ FantasyGenesis_Midjourney.py
@@ -0,0 +1,319 @@
+# A Fantasy Genesis Nested Lists
+
+# The Lists:
+# Anima
+Anim_1_Sea_Life = ["Mollusk",
+                   "Crab, Lobster",
+                   "Squid, Mudskipper",
+                   ["Fish: Deep sea", "Fish: Fresh water"],
+                   "Jellyfish",
+                   [["Whale", "Killer Whale", "Narwhale"], "Dolphin", "Walrus", "Otter"],
+                   ["Shell: spiral", "Conch", "Shell: ribbed, clam"],
+                   "Eel, Leech, Hagfish",
+                   "Coral, Anemone",
+                   "Shark, ray"]
+
+Anim_2_Insect = ["Worm",
+                 "Ant",
+                 "Mosquito",
+                 "Moth, Butterfly",
+                 "Fly, dragonfly",
+                 "Lotus, Mantis",
+                 "Bee, Wasp",
+                 "Caterpillar, Centipede, Milipede",
+                 "Beetle, Scarab",
+                 "Flea, Mite",
+                 "Spider"]
+
+Anim_3_Mammal = ["Sheep, cow",
+                 "Mouse, Rabbit",
+                 "Pig, Boar",
+                 "Deer, Pronghorn",
+                 "Ram, Bull, Buck",
+                 "Elephant, Giraffe",
+                 "Bat",
+                 "Bear",
+                 "Lupine: Wild Dog",
+                 "Horse, Zeebra",
+                 "Feline: Wild Cat",
+                 "Primate"]
+
+Anim_4_Reptile = ["Crocodile, Gila",
+                  "Frog, Newt",
+                  ["Lizard", "Chameleon", "Gecko", "Iguana", "Frilled Lizard"],
+                  ["Snake", "Cobra", "Python", "Rattlesnake"],
+                  ["Turtle", "Box Turtle", "Sea Turtle"]]
+
+Anim_5_Bird = ["Wild Fowl, Duck, Goose",
+               "Farm Fowl, Rooster",
+               "Seabird, Penguin, Gull",
+               "City bird: Raven, Sparrow",
+               "Tropical bird: Parrot, Heron",
+               "Bird of Prey: Hawk, Owl",
+               "Hummingbird"]
+
+# Veggie
+
+Vegi_1_Plant = ["Seaweed",
+                "Fern",
+                "Desert Cacti",
+                "Thick Leaf Plant, Jade",
+                "Flower: Domestic",
+                "Vine",
+                "Poppy",
+                "Grass, Dandelion",
+                "Bamboo",
+                "Flower: Wild",
+                "Carnivorous Plant"]
+
+Vegi_2_Fruit_Vegi = ["Asparagus",
+                     "Pinecone",
+                     "Berry, Grapes",
+                     "Ginger",
+                     "Tree fruit (apple, orange)",
+                     "Bean",
+                     "Pumpkin, Gourd",
+                     "Broccoli, Artichoke",
+                     "Corn",
+                     "Grain, Wheat",
+                     "Pineapple"]
+
+Vegi_3_Fungi = ["Moss",
+                "Slime Fungi: Ooze, Jelly",
+                ["Lichen", "Lichen: Crustose", "Lichen: Foliose", "Lichen: Fruticose"],
+                ["Mushroom: Cap and Stem", "Fungus: Bracket or Shelf", "Fungus: Puffball", "Coral Fungus", "Stinkhorn fungus"]]
+
+Vegi_4_Tree = ["Willow",
+               "Birch",
+               "Maple, Oak",
+               "Banyan",
+               "Pine",
+               "Palm"]
+
+
+
+# Elemental & Mineral
+
+Elem_1_Fire_Electric = [["Fire: campfire", "Fire: candle flame", "Fire: forest fire"],
+                        "Steam: Vapor or mist",
+                        ["Forked Lightning", "Sheet Lightning"],
+                        "Glowing Ember, Hot Coal",
+                        ["Flowing Molten Lava", "Lava Fountain"]]
+
+Elem_2_Liquid = ["Icicles",
+                 "Fog, Vapor",
+                 "Wave",
+                 "Dew Drops",
+                 "Ripple",
+                 "Frost, Snow",
+                 "Suds, Bubbles",
+                 "Tar, Gum"]
+
+Elem_3_Earth_Metal = ["Malachite",
+                      "Mountain, Cliff Face",
+                      "Brick, Cobblestone",
+                      "Rust, Oxide",
+                      "Cracked Clay",
+                      "Stalactite, Stalagmite",
+                      "Glass, Crystals",
+                      "Powder, Sand",
+                      "Slate, Shale",
+                      "Cement, Sediment",
+                      "Mercury, Chrome"]
+
+Elem_4_Astral_Atmosphere = [["Full Moon", "Crescent Moon", "Gibbous Moon"],
+                            ["Starfield: Constellations", "Starfield: Milky Way"],
+                            "Crater, Asteroid",
+                            "Solar Flare: Plasma Loops",
+                            "Galaxy form, spiral or elliptical",
+                            "Volcano: Ash Plume",
+                            ["Planets, Saturn's Rings","Planets, Jupiter's Great Red Spot"],
+                            ["Clouds", "Cyclone", "Turbulence"]]
+
+#Technical
+
+Tech_1_Transportation = ["Car, Truck, Bus",
+                         "Aircraft",
+                         "Rail, Train, Trolley",
+                         "Cycle (motor or bi)",
+                         "Sled, Ski",
+                         "Boat, Ship",
+                         "Spacecraft",
+                         "Tank, Caterpillar Tread"]
+
+Tech_2_Architecture = [["Ornament, Gargoyle", "Ornament, Pillar"],
+                       "Bridge, Framework",
+                       "Castle, Dome",
+                       "Modern Skyscraper",
+                       ["Place of Worship", "Totem", "Cathedral", "Temple"],
+                       ["Doorway, Archway", "Window"],
+                       "Old Village, Cottage",
+                       "Tent"]
+
+Tech_3_Tool = ["Drill",
+               ["Cup", "Bowl", "Plate", "Silverware"],
+               "Umbrella",
+               "Bundle, Bale",
+               "Hammer, Axe",
+               "Brush: Hair, Tooth",
+               "Razor, Knife",
+               "Spigot, Faucet",
+               "Rope",
+               "Lock, Key",
+               "Adhesive, Bandage",
+               "Shovel, Pick",
+               "Capsule, Tablet",
+               "Nuts, Bolts",
+               "Chain",
+               "Thread, Stitch",
+               "Shears, Scissors",
+               "Pen, Paintbrush",
+               "Spring, Coil",
+               "Syringe",
+               "Tube, Plumbing",
+               "Wrench, Pliers"]
+
+Tech_4_Machine = [["Switch", "Dial, Knob", "Button", "Lever", "Foot Pedal"],
+                  ["Bulb, Lamp", "Arc lamp", "Spotlight"],
+                  ["Clock, Gears", "Piston, Cylinder"],
+                  ["Mechanical Saw","Laser Beam"],
+                  ["Reactor Core", "Engine", "Solar Panel"],
+                  "Telephone",
+                  ["Microchip", "Circuit Board", "Network Room, Cables"],
+                  "Dish Antenna",
+                  ["Rocket", "Turbine", "Fan, Propeller"]]
+
+#Character Elements
+
+Emotions = ["Embarrassed",
+            "Anger",
+            "Timid, Bashfull",
+            "Giggle, Smiling",
+            "Squint, Wink",
+            "Bored",
+            "Stressed, Fatigued",
+            "Fear",
+            "Thought, Meditation",
+            "Deadpan",
+            "Insane, Berserker",
+            "Insane, Happy",
+            "Pining, Furrowed",
+            "Laughing, Hysterical",
+            "Attentive, Shock",
+            "Stern, Grumpy",
+            "Clenched Teeth",
+            "Gape, Gawk",
+            "Relief",
+            "Sneering",
+            "Paranoid, Shifty",
+            "Bliss, Joy",
+            "Confusion"]
+
+Actions = ["Recoil, Akimbo",
+           "Drenched, Thirst",
+           "Blown by Cyclone",
+           "Push, Pull",
+           "Snoop, Listen",
+           "Crouched for Attack",
+           "Hang, Climb",
+           "Recoil, Head/Torso",
+           "Float, Levitate",
+           "Swinging Weapon",
+           "Twisting, Stretching",
+           "Kicking, Punching",
+           "Squeeze, Tackle",
+           "Absorb, Eat",
+           "Limp, Injured",
+           "Curse, Swear",
+           "Run, Jump",
+           "Melt, Glow, Fire",
+           "Stuck, Trapped",
+           "Pull, Push",
+           "Shoot Weapon",
+           "Dying, Gaunt",
+           "Fly, Swim",
+           "Shed, Molting",
+           "Chant, Recite",
+           "Crawl, Emerge"]
+
+# Compliations of the different Sets
+
+Anima = [Anim_1_Sea_Life,
+        Anim_2_Insect,
+        Anim_3_Mammal,
+        Anim_4_Reptile,
+        Anim_5_Bird]
+
+EleMineral = [Elem_1_Fire_Electric,
+              Elem_2_Liquid,
+              Elem_3_Earth_Metal,
+              Elem_4_Astral_Atmosphere]
+
+Veggie = [Vegi_1_Plant,
+          Vegi_2_Fruit_Vegi,
+          Vegi_3_Fungi,
+          Vegi_4_Tree]
+
+Techne = [Tech_1_Transportation,
+          Tech_2_Architecture,
+          Tech_3_Tool,
+          Tech_4_Machine]
+
+Visual_Elements = [Anima, Veggie, EleMineral, Techne]
+
+Character_Elements = [Emotions, Actions]
+
+All_Things = [Visual_Elements, Character_Elements]
+
+from random import choice
+
+def RecurseChoice(Root):
+    # Randomly select from nested lists until you get to
+    # a string instead of a list. Return the string.
+    if isinstance(Root,str):
+        return Root
+    else:
+        Sub_Root = choice(Root)
+        return RecurseChoice(Sub_Root)
+
+#Choose one from each category
+'''
+print("Anima: ", end='')
+print(RecurseChoice(Anima))
+print("Veggie: ", end='')
+print(RecurseChoice(Veggie))
+print("EleMineral: ", end='')
+print(RecurseChoice(EleMineral))
+print("Techne: ", end='')
+print(RecurseChoice(Techne))
+
+print("Emotions: ", end='')
+print(RecurseChoice(Emotions))
+print("Actions: ", end='')
+print(RecurseChoice(Actions))'''
+
+visual = RecurseChoice(Visual_Elements).replace(": ","-").replace(", ","-").replace(" ","-")
+style = RecurseChoice(Visual_Elements).replace(": ","-").replace(", ","-").replace(" ","-")
+mood   = RecurseChoice(Character_Elements).replace(": ","-").replace(", ","-").replace(" ","-")
+
+prompt = f"form of {visual} made of {style} is {mood}, in the style of concept art, fantasy genesis --s 150 --ar 16:9 --c 5 --q 0.5"
+print(prompt)
+
+try:
+    import pyperclip
+    pyperclip.copy(prompt)
+except:
+    pass
+
+# Some test stuff
+
+# Run it a ton of times looking for an entry
+# or just checking for a crash, that too.
+'''
+Search_target = "Narwhale"
+Number_of_tries = 4000
+for i in range(Number_of_tries):
+    result = RecurseChoice(All_Things)
+    if result == Search_target: print("Target found! Try #{}".format(i))
+
+'''
