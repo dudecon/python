19b3c2db0b48c4a1c34f2553fe740d7f6b182463
Paul Spooner <dudecon@hotmail.com>
2024-05-25 14:08:32 -0400

    added functionality
    backup folder creation, resizing, colorspace, and file renaming

--- ImageFormatConverter.py
+++ ImageFormatConverter.py
@@ -1,18 +1,41 @@
-from os import rename, listdir
+from os import rename, listdir, makedirs
 from PIL import Image
 
 thesefiles = listdir()
 target = ".png"
+target_size = 800 # max dimension of image, < 1 means no resize
 new = ".jpg"
-newdir = "./PNGs/"
+colorspace = 'RGB' # 'L' is black and white, 'RGB' is color
+filename = "Laser_Decoration_Design " # set to "" to not rename files
+
+
+newdirname = "Original_"+target[1:].upper()+"s"
+newdir = f"./{newdirname}/"
 tlen = len(target)
+if not newdirname in thesefiles:
+    makedirs(newdir)
+i = 0
 for f in thesefiles:
     if f[-tlen:] == target:
+        i += 1
         image = Image.open(f)
-        image = image.convert('RGB')
-        n = f
-        n = n[:-tlen] + new
-        image.save(n)
-        m = newdir + f
-        rename(f, m)
+        image = image.convert(colorspace)
+        if target_size >= 1:
+            cursize = (image.width, image.height)
+            maxdim = max(cursize)
+            scale = target_size/maxdim
+            if scale < 1:
+                newdim = (int(i*scale) for i in cursize)
+                image = image.resize(newdim)
+        if len(filename):
+            name = filename + str(i) + new
+        else:
+            name = f[:-tlen] + new
+        image.save(name)
+        if True: #move the originals to the backup folder
+            m = newdir + f
+            rename(f, m)
         print("processed", f)
+    else:
+        pass
+        #print(f)
