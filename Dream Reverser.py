31f54a037e0fb46929fd1bd51c4cd79d2c804c78
Paul Spooner <dudecon@hotmail.com>
2025-05-03 07:11:16 -0700

    Create Dream Reverser.py

---
+++ Dream Reverser.py
@@ -0,0 +1,91 @@
+dreamtext = '''2025-01-10
+((in a real-life WoW Ironforge. Kinda sad museum exhibit/playplace recreation. Shift to somewhere else, doing something else? Can’t recall.))
+
+2025-01-20
+I am in a big manor house. (The new house on Montgomery?) It feels like a Victorian style building. I have the kids with me, mostly little kids. We walk in a loose group down the wide spiral stairs and out into the foggy yard. The short-cropped grass is prickly underfoot. A tall wrought-iron fence surrounds the yard, though it shifts to plasma cut plate steel as I examine it. The steel is a dull ochre, as if it has been painted to look rusty, with a clear-coat over the top. I think to myself, in the dream, that it would have been easier for them to let it rust and then hit it with a sander.
+I and the children - and a woman attendant - open a gate and walk further from the house, out into another fenced yard. There are aggressive dogs outside the fence. I sense in the dream that they are barking and growling, but hear nothing. They jump at the fence, and most of the little kids run over with delight to look at them. One of the smallest toddlers, Ruth or Evelyn or Adelaide, takes my hand or otherwise walks with me further along. The fence is to our right, and after 70 feet or so gives way to a swing-set. I think in the dream that the dogs could simply run around the fence, but somehow this is irrelevant. The attendant - an older overweight short woman who has a waddle in her walk - follows the toddler to the swings.
+There is another woman there as well, who walks with me. She reminds me of Anna my wife, but also of Anna Stephens that I was friends with as a child. We cross from the yard into the road, for there is no fence beyond the swing-set, and continue to walk further from the manor. Presently we are walking through an interior, it feels like a protestant “homeschool house” in that it is both sparse and disheveled. I ask several times “What is this place?” but receive no response. The young woman leads me a little to the right, to a door. As she places her hand on the knob and opens the door to the bedroom, the dream slows and darkens. I whisper to her, again and again, “This is a dream”, and I feel that it is not mine but hers, and that I have somehow come in contact with the soul of Anna Stephens in purgatory. I awake slowly.
+
+2025-01-21
+A repeating dream which runs at least three times. It appears to be set in the game “The Last Starship” and involves loading a game, and then most of the ship getting shot to pieces. All that is left is a block about 6 tiles high and a couple dozen tiles wide, which is also getting hit with fire, but manages to hold together much longer.
+
+2025-01-25
+A very disjointed dream, which only bears recording because I wrote down a note asking if I should record it. I can recall no details, other than something about the interior of a building and parallel shafts of yellow light.
+
+
+2025-01-25
+A vague disjointed dream about which I can recall no detail.
+
+2025-01-27
+A vague Dyson Sphere Project style dream. Spaceships and orbits, swarm satellites orbiting in the void. I disassemble them, and then put them back together, again and again. A countdown. Airlocks. Communications. I awake slowly.
+
+2025-01-28
+A vague dream (except for the vivid ending). I am on a brightly colored island, perhaps playing a base building game. Then we are on a boat, out over the deep water.
+I am swimming in the aquamarine water. High in the water at first and then lower and lower as waves wash over me. I am sinking fast, feet downward, trying feebly to swim. I can tell I am still breathing, but I am drowning in the dream. The sea around me grows dark, and the dissonance of breathing this deep underwater causes me to awake. Afterwards I am a bit disturbed and frightened.
+
+2025-02-09
+I am playing remotely in a board-game version of Song of the Shattered Isle which takes the form of a chess tournament. I am playing the game remotely on my computer, and there is a female opponent who reminds me of the female antagonist from the Deadpool and Wolverine movie, especially with what comes next with the reality streams and things.
+The dream shifts a little bit, and I’m now in the Girl Genius universe looking at a big stone steampunk archway with a magic portal in the middle. The archway is set on a knoll in the middle of a ruinous waste, ancient stonework scattered here and there amid sickly tufts of grass and discarded machinery.
+Baron Wulfenbach comes through the portal and stands in front of it in a dramatic pose. Behind him the female antagonist pulls up the corners/posts of the portal and blasts the Baron in the back of the head with the energy streams. The magic energy glances off his head and freezes him in time. I feel like I should go fight the bad lady, but time itself is starting to unravel. I awake somewhat confused.
+
+2025-02-10
+A driving dream, through a somewhat rural area. I have gas and am shifting in my seat to fart frequently. I poop my pants a little, and spend the rest of the dream holding myself up on the back of my shoulders and thighs against the seat. I think I’m looking for a fast-food restaurant, but when I find one and pull in to the parking lot I don’t know what to do, so I just awake.
+
+2025-02-11
+A long involved dream about… something? I can’t recall any details.
+
+2025-02-12
+I am at work, in a big windowless warehouse with many rows of desks (very much like CFS). I recline in my chair, somewhat like a dentist chair. A weirdo from work - overweight and heavily mouth-breathing - is whispering nonsense nearby. I get up from my chair and look through the many devices on the desks until I find a hand-held high power laser crossed with a camcorder. The device is in two pieces, with a VCR-like power pack and a pistol-like handheld portion with a wide round aperture about a hand-span across. I point it down the row of desks - maybe at the weirdo - and turn it on by pressing the button with the round red “record” button on it. The laser sets a fire. I sense this is bad, so I turn the laser off. But now there is thick white smoke pouring from some of the electrical components on the desk. I try to call for help, but no one is listening. I bend the dream to put out the fire, but then the whole dream erupts in flames and I awake.
+
+2025-02-21
+(while I was in London, the last day of ARC)
+The dream begins, as always, in a vague fractal of ambiguity. Our house is perched on a hilltop, the landscape is rugged and rocky. I am on an adjacent hill, separated from the house by a deep pit. I hear Anna calling to me from the house, or perhaps I see something, water. I begin to run. In the valley to my left there is a dam-like structure, with a sheer concrete face. I descend the concrete, grabbing hold of protrusions, leaping downward. I reach a landing, a series of prefab concrete slabs jutting out from the sides of the pit. I can see there is a waterfall emerging from the house, or perhaps from the garage. I jump the gaps between the slabs, and Anna descends the staircase to meet me at the edge of the pit.
+“How long has this been happening?” I ask, referring to the waterfall.
+“It’s always been like this.” she responds in dismay, “Connie” in the dream the former owner of the house “knew about it but never fixed it.”
+As we are trying to figure out what to do, I awake.
+
+2025-03-03
+A vague erotic dream. At the end, Anna is straddling me, rubbing her labia on my penis. “Ten more seconds” she says, and then counts down much too quickly. When she reaches zero she gets up and walks away. I catch a glimpse of her naked back before the dream ends.
+
+2025-03-20
+I’m sitting on a couch in a dimly lit living room or basement, talking with someone. We are discussing or perhaps narrating how the wargaming session is going. The view shifts to down on the floor, where the players are sitting crosslegged on the carpet amidst their miniatures. The primary player, or game master, or ludic antagonist perhaps, he is controlling a huge (for a wargaming miniature anyway, about as long as my arm) plastic turret, which he directs to slowly turn to point at the various players. I get an “eye of Sauron” vibe from the turret, though it looks vaguely futuristic, organic, and injection molded, like a Nerf gun perhaps. The GM also strikes me as vaguely menacing, reminds me of Rutskarn. The conversation continues, and the dream dissolves into vagueness. I awake some time later.
+
+2025-03-22
+A vague dream about (un)packing and laundry. I had just done a load of laundry the day before (in the waking world) and in the dream I had too many pillow-cases or pillows or something. I kept trying to pack them in with my shoes or something, and someone was helping me, and the dream kept getting more and more packed with people and pillows until I awoke.
+
+2025-03-24
+I am walking the Elysian Fields, the Paradise of God. As I walk, I am reunited with my children at the playground. They have dug out a waterfall from the wall of the playground area, and the exposed concrete that they have excavated sparkles with white thumb-sized crystals. I sit down on a bench with Anna, and am overwhelmed with emotion at our reunion. I lay sobbing in her bosom until I fall asleep.
+Later I awake, or have a dream within the dream. I meet three crowds or processions, each of about two dozen people, all finely dressed in noble medieval garments. I see Mothers, then Kings/Wizards, then Matriarchs. Then I awoke.
+On recalling the dream, I felt the further elaboration was appropriate: The first is Mothers, talking together while their many children gambol, run, squeal, and play around them. Next are Kings and Wizards, singing while their retinue of Warriors patrol in wide flanks and the Fathers follow close behind. Last are the Matriarchs, attended by Maidens as they proceed in stately silence.
+
+2025-04-10
+(had the conversation with Nadia (connected to ARC) the night before, she was at a Google startup conference room)
+((I’m at a casino combined with a conference center. It is casino-like from all the slot machines everywhere. The conference center feel reminds me of the central concourse at the EXCEL conference center in London where ARC was held. There are some gay bros there, and one of them wedges his cell-phone in my buttcrack and takes a picture(s). I confrontationally ask him “Do I need to call security?” and he backs off embarrassed. One of his buddies (taller) comes up to me and tries to get me interested in the pictures, so I ask him the same thing and he backs off too.))
+
+2025-04-12
+((combined room of my bedrooms in Hawthorn House and the Montgomery House. I am sleeping in separate beds from Anna))
+
+2025-04-22
+(the night before I start at Mcallister, and the night previous Anna went into labor with the molar pregnancy)
+((First person game. Starts out in the old mansion. Playing with the kids. They run off ahead, haunted mansion. I find a secret panel that opens to a staircase with spooky sounds. I’m afraid Anna ran off into that area. Several other game sections, all feel familiar, Korean MMORPG. I want to play with Anna, so I run a teleport command and wake up next to her in bed.))
+
+2025-04-29
+I am driving in Boston, in the city, with narrow twisty roads, like on Bunker Hill. I park the car, and we walk through an archway or building. The dream shifts hazily between walking beside low wrought iron fences, and the kids running off onto small grassy parks. Eventually I start gathering them to walk back to the car, but I can only find a few. We walk back to the car anyway, and I start driving around to try to pick up the rest of the children, but they are proving difficult to locate when I awake.
+
+2025-04-30
+A vague disjointed strange dream, about which I fail to recall any significant details.
+
+2025-05-02
+I am back in the haunted video game. This time it is a pirate ship, first person, and kind of reminds me of the pirate boss in “Realm of the Mad god”. The hidden door opens, and the stairway up (?) into the darkness and the slide down into the pitch black hold beckon. I don’t want to go in there. I really don’t want to. I start interacting with a female, who is attempting to convince and/or castrate me? The dream dissolves.
+
+I sleep again, and climb into my parent’s station wagon. I’m clambering around in the trunk or the back seats. There in the middle of the car is a small nest bed with a ferret or maybe a weasel curled up and asleep. The dream is bright, sepia tone, and warm. Too warm, and I awake a little overheated.
+
+I sleep again, and dream a third time, of interacting with my phone which is now a laptop sized (folding?) tablet thanks to a software update which also made the UI a windows-based system. I keep dragging windows around and my phone fills more and more of my view until the dream recurses into the phone, breaks, and I awake.
+'''
+sep = "2025"
+splits = dreamtext.split(sep)
+
+splits.reverse()
+
+print(sep.join(splits))
+pass
